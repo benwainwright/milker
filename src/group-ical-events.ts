18ec84a708c7a91e21b2202dd46617a01cd243ed
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-10-07 10:39:44 +0100

    ignore all day events

--- src/group-ical-events.ts
+++ src/group-ical-events.ts
@@ -2,6 +2,32 @@ import { CalendarComponent, FullCalendar } from "ical";
 import { Day } from "./day";
 import { DateTime } from "luxon";
 
+const isAllDayEvent = (event: CalendarComponent) => {
+  if (!event.start || !event.end) {
+    return false;
+  }
+
+  const startTime = DateTime.fromJSDate(event.start).toLocaleString(
+    DateTime.TIME_24_WITH_SECONDS,
+  );
+  const endTime = DateTime.fromJSDate(event.end).toLocaleString(
+    DateTime.TIME_24_WITH_SECONDS,
+  );
+
+  const diff = DateTime.fromJSDate(event.end)
+    .diff(DateTime.fromJSDate(event.start), "days")
+    .toObject();
+
+  if (
+    startTime === "00:00:00" &&
+    endTime === "00:00:00" &&
+    (diff.days ?? 0) > 0
+  ) {
+    return true;
+  }
+  return false;
+};
+
 export const groupIcalEvents = (calendar: {
   [key: string]: CalendarComponent;
 }): Day[] => {
@@ -12,6 +38,10 @@ export const groupIcalEvents = (calendar: {
       const date = DateTime.fromJSDate(value.start).toLocaleString(
         DateTime.DATE_FULL,
       );
+
+      if (isAllDayEvent(value)) {
+        return accum;
+      }
 
       if (date in accum) {
         return { ...accum, [date]: [...accum[date], value] };
