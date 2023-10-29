68b167723d623ec3ac72276dbe664b88f686c05e
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-10-29 16:33:16 +0000

    store the token in flat json

--- src/run.ts
+++ src/run.ts
@@ -5,6 +5,7 @@ import { RtmClient } from "./lib/rtm/client";
 import { AppStorage } from "./types/storage";
 import { TerminalTokenRequester } from "./lib/rtm/terminal-token-requester";
 import { getEnv } from "./lib/core/get-env";
+import { FlatFileJsonStorage } from "./lib/rtm/flat-file-json-storage";
 
 // const BEN_ICAL =
 //   "https://calendar.google.com/calendar/ical/e50f1bef70739f9f0a825126a7f8fed4f8a26d134060969f762330608667d647%40group.calendar.google.com/private-0fad6b4bf9797ac58d4e41f94aa1516c/basic.ics";
@@ -27,7 +28,7 @@ const run = async () => {
   // //     value.start && DateTime.fromJSDate(value.start) > DateTime.now(),
   // // );
   // await writeFile(`${cwd()}/data.json`, JSON.stringify(parsed));
-  const storage = {} as AppStorage;
+  const storage = new FlatFileJsonStorage(`${process.cwd()}/token.json`);
   const tokenRequester = new TerminalTokenRequester();
   const key = getEnv("MILK_API_KEY");
   const secret = getEnv("MILK_SHARED_SECRET");
