1252c37cb71b5cf0b718c8aae2bbe72263907b91
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-10-30 21:12:13 +0000

    vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
       all: true,
       include: ["src/lib/**/*.ts"],
       provider: "istanbul",
+      cleanOnRerun: true,
+      thresholdAutoUpdate: true,
+      lines: 81.49,
+      branches: 79.31,
+      functions: 87.14,
+      statements: 81.57,
     },
   },
 });
